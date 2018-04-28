d9ee6cd8826645ba9e4993a2257454f8d3093483
github username <github email address>
2018-04-27 21:51:11 -0400

    moved key into .env

--- nyc-recycles-client/src/components/MapContainer.js
+++ nyc-recycles-client/src/components/MapContainer.js
@@ -2,17 +2,11 @@ import React from "react";
 import Map from "./Map";
 import { GoogleApiWrapper } from "google-maps-react";
 
-let apiKey = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
-
 class MapContainer extends React.Component {
   render() {
     console.log(this.props.google);
-    const style = {
-      width: "50vw",
-      height: "50vh"
-    };
     return (
-      <div style={style}>
+      <div>
         <Map google={this.props.google} />
       </div>
     );
@@ -20,5 +14,5 @@ class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyDCvMjhwAUggY8VmB8seyipG49EzJ7uLs0"
+  apiKey: process.env.GOOGLE_API_KEY
 })(MapContainer);
